56c6554ff2c058627d2c00c2737c7817ed0b8e87
Blake <blakepetersont@gmail.com>
2022-12-19 12:28:23 -0700

    condesing the amount of files I am using. Also adding the data for the portfolio

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Route, Redirect, HashRouter } from "react-router-dom";
 import Navigation from "../../components/navigation";
-import About from "../../components/about";
 import Portfolio from "../../components/portfolio";
 import Contact from '../../components/contact';
 import Resume from '../../components/resume';
@@ -25,6 +24,38 @@ class Header extends Component {
       </HashRouter>
     );
   }
-}
+};
+
+function About() {
+
+  return(
+      <section className="container">
+          <h2 class="top-title">Blake Peterson</h2>
+          <hr></hr>
+          <div>
+              <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/108911520?v=4" alt="Blake Peterson" />
+
+              <p>
+              
+              </p>
+              <p>
+              
+              </p>
+              <p>
+              
+              </p>
+          </div>
+      </section>
+  )
+};
+
+function Footer() {   
+  return (
+  <footer className="footer bg-dark fixed-bottom">
+      <a href="https://github.com/Blake1210"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+      <a href="https://www.linkedin.com/in/blake-peterson-61b11a158/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+  </footer>
+  )
+};
 
 export default Header;
